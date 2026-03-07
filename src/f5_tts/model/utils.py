7781f1d1465bb643556fe8afbd491474e0c7e046
lovench415 <97780774+lovench415@users.noreply.github.com>
2026-03-07 11:48:48 +0900

    Add files via upload

--- src/f5_tts/model/utils.py
+++ src/f5_tts/model/utils.py
@@ -13,7 +13,7 @@ from pypinyin import lazy_pinyin, Style
 #from g2pk import G2p
 #from korean_romanizer.romanizer import Romanizer
 # seed everything
-from jamo import h2j, hangul_to_jamo
+
 
 
 #g2p=G2p()
@@ -183,69 +183,6 @@ def convert_char_to_pinyin_orig(text_list, polyphone=True):
                         char_list.extend(lazy_pinyin(c, style=Style.TONE3, tone_sandhi=True))
                     else:
                         char_list.append(c)
-        final_text_list.append(char_list)
-
-    return final_text_list
-
-
-# modifited the original convert_char_to_pinyin in order to convert from "hangul" to "jamo" discreting 'cho sung' and 'jong sung'
-def convert_char_to_pinyin(text_list, polyphone=True):
-    
-    final_text_list = []
-    god_knows_why_en_testset_contains_zh_quote = str.maketrans(
-        {"“": '"', "”": '"', "‘": "'", "’": "'"}
-    )
-    custom_trans = str.maketrans({";": ","})
-
-    for text in text_list:
-        char_list = []
-        ko_buffer = []  
-        text = text.translate(god_knows_why_en_testset_contains_zh_quote)
-        text = text.translate(custom_trans)
-
-        for seg in jieba.cut(text):  
-            seg_byte_len = len(bytes(seg, "UTF-8"))
-
-            if all("가" <= char <= "힣" for char in seg): 
-                if not ko_buffer and char_list and char_list[-1] != " ":
-                    char_list.append(" ")
-                ko_buffer.append(seg)
-
-            else:  
-                if ko_buffer:  
-                    joined_ko = "".join(ko_buffer)  
-                    ko_buffer = []  
-                    #converted_ko = g2p(joined_ko)
-                    converted_ko = hangul_to_jamo(joined_ko)
-                    char_list.extend(list(converted_ko))
-
-                if seg_byte_len == len(seg):  
-                    if char_list and seg_byte_len > 1 and char_list[-1] not in " :'\"":
-                        char_list.append(" ")
-                    char_list.extend(seg)
-                elif polyphone and seg_byte_len == 3 * len(seg):  
-                    seg = lazy_pinyin(seg, style=Style.TONE3, tone_sandhi=True)
-                    for c in seg:
-                        if c not in "。，、；：？！《》【】—…":
-                            char_list.append(" ")
-                        char_list.append(c)
-                else:  
-                    for c in seg:
-                        if ord(c) < 256:
-                            char_list.extend(c)
-                        else:
-                            if c not in "。，、；：？！《》【】—…":
-                                char_list.append(" ")
-                                char_list.extend(lazy_pinyin(c, style=Style.TONE3, tone_sandhi=True))
-                            else:  
-                                char_list.append(c)
-
-        if ko_buffer:
-            joined_ko = "".join(ko_buffer)
-            #converted_ko = g2p(joined_ko)
-            converted_ko = hangul_to_jamo(joined_ko)
-            char_list.extend(list(converted_ko))
-
         final_text_list.append(char_list)
 
     return final_text_list
